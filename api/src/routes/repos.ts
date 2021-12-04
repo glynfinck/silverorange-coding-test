150f6356d56880283b8ab3fe001f1b1ff6a764fe
Glyn Finck <glynfinck97@gmail.com>
2021-12-04 11:17:47 -0800

    the endpoint /repos returns all respositories, adding filter functionality for only unforked repos now

--- api/src/routes/repos.ts
+++ api/src/routes/repos.ts
@@ -1,12 +1,18 @@
 import { Router, Request, Response } from 'express';
+import axios from 'axios';
 
 export const repos = Router();
 
 repos.get('/', async (_: Request, res: Response) => {
-  res.header('Cache-Control', 'no-store');
+  // 1) get all repos
+  const response = await axios.get(
+    'https://api.github.com/users/silverorange/repos'
+  );
+  const repositories = response.data;
 
+  res.header('Cache-Control', 'no-store');
   res.status(200);
 
   // TODO: See README.md Task (A). Return repo data here. You’ve got this!
-  res.json([]);
+  res.json(repositories);
 });
